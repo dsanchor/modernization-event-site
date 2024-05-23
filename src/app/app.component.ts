361d8a721ee595de5c6bd829c64ccb29830de149
dsanchor <sancheski@gmail.com>
2024-05-23 22:13:11 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Modernization Event 2024';
+  title = 'Microsoft Spain';
 }
